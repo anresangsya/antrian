ec29ac896a3be7482df7f135ef4be62642209790
Anre Sangsya <ekaanresangsya@gmail.com>
2017-12-06 14:24:32 +0700

    login

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,7 @@ router.post('/login', function(req, res, next) {
 	var email = req.body.email,
 		password = req.body.password;
 
-	db.query("SELECT u.*, p.id_pasien, p.nama_pasien, p.tgl_lahir, p.telepon, p.alamat, p.bpjs FROM users u LEFT JOIN pasien p ON u.email=p.email WHERE u.email=?", [email], function(error, results, fields) {
+	db.query("SELECT u.*, p.id_pasien, p.nama_pasien, p.tgl_lahir, p.jk, p.telepon, p.alamat, p.bpjs FROM users u LEFT JOIN pasien p ON u.email=p.email WHERE u.email=?", [email], function(error, results, fields) {
 		if(error){
 			res.send({
 		      "status" : 400,
